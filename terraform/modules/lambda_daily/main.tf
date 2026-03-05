013d1e6b7a049ff6d1d8dbb332196a90581820fd
Luan Borba <luanborba35@gmail.com>
2026-03-05 20:14:33 -0300

    infraestrutura lambda_daily

---
+++ terraform/modules/lambda_daily/main.tf
@@ -0,0 +1,80 @@
+locals {
+  lambda_name = "${var.project_name}-${var.environment}-lambda-daily"
+  bucket_name = "${var.project_name}-${var.environment}-daily-files"
+}
+resource "aws_s3_bucket" "daily_bucket" {
+  bucket = local.bucket_name
+}
+resource "aws_iam_role" "lambda_role" {
+  name = "${local.lambda_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.daily_bucket.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+resource "aws_lambda_function" "daily_lambda" {
+  function_name = local.lambda_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.handler"
+  runtime       = "python3.10"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.daily_bucket.bucket
+    }
+  }
+}
+resource "aws_cloudwatch_event_rule" "daily_rule" {
+  name                = "${local.lambda_name}-rule"
+  schedule_expression = "cron(0 10 * * ? *)"
+}
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_rule.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.daily_lambda.arn
+}
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.daily_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_rule.arn
+}
