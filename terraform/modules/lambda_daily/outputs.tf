6b9070bebb39bd2aeb82b45dc05427958f2c4e74
Luan Borba <luanborba35@gmail.com>
2026-03-05 20:10:14 -0300

    valores de saida do módulo lambda_daily

--- terraform/modules/lambda_daily/outputs.tf
+++ terraform/modules/lambda_daily/outputs.tf
@@ -1,3 +1,7 @@
-output "lambda_daily_bucket" {
-  value = module.lambda_daily.lambda_daily_bucket_name
+output "scheduler_bucket_name" {
+  value = aws_s3_bucket.daily_bucket.bucket
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.daily_lambda.function_name
 }
