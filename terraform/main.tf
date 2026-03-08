ffb810ff47f99a63f48554e15cdefff334f7f13b
Luan Borba <luanborba35@gmail.com>
2026-03-08 14:42:51 -0300

    main: Update backend module outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,16 @@ module "frontend" {
   environment  = var.environment
 }
 
-module "backend" {
-  source = "./modules/backend"
-
-  project_name = var.project_name
-  environment  = var.environment
-}
-
 module "lambda_daily" {
   source = "./modules/lambda_daily"
 
   project_name = var.project_name
   environment  = var.environment
+}
+
+module "backend" {
+
+  source = "./modules/backend"
+
+  project_name = "luan-backend"
 }
